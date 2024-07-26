a80ca84ed72a4b4ae02e6fdfcf534fb45396dacb
Blaine Heffron <baheffron@gmail.com>
2024-07-26 15:13:45 -0400

    fmt

--- src/log/diagnostic_events.rs
+++ src/log/diagnostic_events.rs
@@ -1,10 +1,11 @@
-use stellar_xdr::curr::{DiagnosticEvent, Limits, ReadXdr};
 use serde_json;
+use stellar_xdr::curr::{DiagnosticEvent, Limits, ReadXdr};
 
-pub fn diagnostic_events(events: &Vec<String>, level: tracing::Level) {
+pub fn diagnostic_events(events: &[String], level: tracing::Level) {
     for (i, event_xdr) in events.iter().enumerate() {
-        let event = DiagnosticEvent::from_xdr_base64(event_xdr, Limits::none()).expect("Failed to decode Diagnostic Event XDR");
-        let json = serde_json::to_string(&event).expect( "JSON encoding failed");
+        let event = DiagnosticEvent::from_xdr_base64(event_xdr, Limits::none())
+            .expect("Failed to decode Diagnostic Event XDR");
+        let json = serde_json::to_string(&event).expect("JSON encoding failed");
         let log_message = format!("{i}: \"{event_xdr:#?}\" {json}");
         match level {
             tracing::Level::TRACE => tracing::trace!("{}", log_message),
