54cd5053bf52a8aba3c4a7cca99f9c773f4418a3
Blaine Heffron <baheffron@gmail.com>
2024-07-26 15:10:49 -0400

    add diagnostic event decode

--- src/log/diagnostic_events.rs
+++ src/log/diagnostic_events.rs
@@ -1,11 +1,16 @@
-pub fn diagnostic_events(events: &[impl std::fmt::Debug], level: tracing::Level) {
-    for (i, event) in events.iter().enumerate() {
-        if level == tracing::Level::TRACE {
-            tracing::trace!("{i}: {event:#?}");
-        } else if level == tracing::Level::INFO {
-            tracing::info!("{i}: {event:#?}");
-        } else if level == tracing::Level::ERROR {
-            tracing::error!("{i}: {event:#?}");
+use stellar_xdr::curr::{DiagnosticEvent, Limits, ReadXdr};
+use serde_json;
+
+pub fn diagnostic_events(events: &Vec<String>, level: tracing::Level) {
+    for (i, event_xdr) in events.iter().enumerate() {
+        let event = DiagnosticEvent::from_xdr_base64(event_xdr, Limits::none()).expect("Failed to decode Diagnostic Event XDR");
+        let json = serde_json::to_string(&event).expect( "JSON encoding failed");
+        let log_message = format!("{i}: \"{event_xdr:#?}\" {json}");
+        match level {
+            tracing::Level::TRACE => tracing::trace!("{}", log_message),
+            tracing::Level::INFO => tracing::info!("{}", log_message),
+            tracing::Level::ERROR => tracing::error!("{}", log_message),
+            _ => tracing::debug!("{}", log_message), // Default to debug for other levels
         }
     }
 }
