113f7719650555faa174de3b33d8197a0f8931ef
Blaine Heffron <baheffron@gmail.com>
2024-07-26 15:56:38 -0400

    do not panic when json decode fails

--- src/log/diagnostic_events.rs
+++ src/log/diagnostic_events.rs
@@ -3,10 +3,18 @@ use stellar_xdr::curr::{DiagnosticEvent, Limits, ReadXdr};
 
 pub fn diagnostic_events(events: &[String], level: tracing::Level) {
     for (i, event_xdr) in events.iter().enumerate() {
-        let event = DiagnosticEvent::from_xdr_base64(event_xdr, Limits::none())
-            .expect("Failed to decode DiagnosticEvent XDR");
-        let json = serde_json::to_string(&event).expect("JSON encoding of DiagnosticEvent failed");
-        let log_message = format!("{i}: {event_xdr:#?} {json}");
+        let event_result = DiagnosticEvent::from_xdr_base64(event_xdr, Limits::none());
+        let json_result = event_result
+            .as_ref()
+            .ok()
+            .and_then(|event| serde_json::to_string(event).ok());
+
+        let log_message = match (event_result, json_result) {
+            (Ok(_), Some(json)) => format!("{i}: {event_xdr:#?} {json}"),
+            (Err(e), _) => format!("{i}: {event_xdr:#?} Failed to decode DiagnosticEvent XDR: {e}"),
+            (Ok(_), None) => format!("{i}: {event_xdr:#?} JSON encoding of DiagnosticEvent failed"),
+        };
+
         match level {
             tracing::Level::TRACE => tracing::trace!("{}", log_message),
             tracing::Level::INFO => tracing::info!("{}", log_message),
